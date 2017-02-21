432804fcb4adec29866a578dca342490c2f50333
Apple <Apple@Apples-MacBook-Pro.local>
2017-02-21 18:23:41 -0500

    first commit

---
+++ JaismeenApp/TaskManager.swift
@@ -0,0 +1,13 @@
+//
+//  TaskManager.swift
+//  JaismeenApp
+//
+//  Created by Apple on 2017-02-21.
+//  Copyright © 2017 proApptive. All rights reserved.
+//
+
+import UIKit
+
+class TaskManager: NSObject {
+
+}
