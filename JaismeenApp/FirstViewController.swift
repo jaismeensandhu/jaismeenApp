fad5edbb6f28b41f2d1bdd3d94baa44bf7cdf8bc
jaismeen.sandhu <jaismeen.sandhu@gmail.com>
2017-02-21 22:48:46 -0500

    Added New Child Item

--- JaismeenApp/FirstViewController.swift
+++ JaismeenApp/FirstViewController.swift
@@ -7,19 +7,65 @@
 //
 
 import UIKit
+import Firebase
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    var data:[String]=["Test","New","One More"]
+    
+    @IBOutlet weak var myTable: UITableView!
+    
+    var quantityCell:[String]=["1","2","3"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return data.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell=tableView.dequeueReusableCell(withIdentifier: "Row", for: indexPath) as! TaskManager
+        
+        let item = cell.viewWithTag(1) as! UITextField
+        item.text=data[indexPath.row]
+        let quantity=cell.viewWithTag(2) as! UILabel
+        quantity.text=quantityCell[indexPath.row]
+        return cell
+    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            self.data.remove(at: indexPath.row)
+            self.myTable.reloadData()
+        }
+    }
+    
+    @IBAction func SaveTable(_ sender: UIButton) {
+        
+        var ref=FIRDatabase.database().reference().child("My ToDo List")
+        let newRef=ref.childByAutoId()
+        newRef.setValue(data)
+    }
+    
+    var searchResults = [[String : AnyObject]]()
+    
+    @IBAction func resetBtn(_ sender: UIButton) {
+        data = []
+        data.removeAll()
+        self.myTable.reloadData()
+        
+    }
+    
+    
 }
 
--- JaismeenApp/TaskManager.swift
+++ JaismeenApp/TaskManager.swift
@@ -2,12 +2,37 @@
 //  TaskManager.swift
 //  JaismeenApp
 //
-//  Created by Apple on 2017-02-21.
+//  Created by Student on 2017-02-21.
 //  Copyright © 2017 proApptive. All rights reserved.
 //
 
 import UIKit
 
-class TaskManager: NSObject {
-
+class TaskManager: UITableViewCell {
+   
+   
+    @IBOutlet weak var Desc: UILabel!
+    @IBOutlet weak var Item: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+   
+    @IBAction func changeStatus(_ sender: UISwitch) {
+    }
+    
+    
+    @IBAction func editItem(_ sender: UIButton) {
+    }
+  
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+    
+    
 }
